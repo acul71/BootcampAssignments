2a441b80e6c8ffe34cfebf58103af698bbf5c708
acul71 <luca.pisani@birdo.net>
2020-08-28 00:33:05 +0200

    Add Ownable.sol

---
+++ Week6/Day5/Ownable.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract Ownable {
+    address internal owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _; // Continue execution
+    }
+}
